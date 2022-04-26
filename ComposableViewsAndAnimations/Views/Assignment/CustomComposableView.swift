d8313fa1634d87b85379dfc72f73214bc0b49818
Leo Lu <llu@lcs.on.ca>
2022-04-26 19:42:53 -0400

    finished the circle view in class

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct CustomComposableView: View {
+    
+//    @State var xOffset = -100.0
+//
+//
+//    @State var rotationAmount = 0.0
+//
+//    let timer = Timer.publish(every: 0.25, on: .main, in: .common).autoconnect()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        
+       ZStack {
+//
+//
+//
+//            Circle()
+//                .frame(width: 50, height: 50)
+//            Text("Ok")
+//                .foregroundColor(.white)
+//        }
+//
+//        .rotationEffect(.degrees(rotationAmount), anchor: .center)
+//        .offset(x:xOffset, y:0)
+//            .animation(
+//                Animation.easeInOut(duration: 2)
+//                    .repeatForever(autoreverses: true)
+//
+//        )
+//
+//        .onReceive(timer) { input in
+//
+//            xOffset = 100.0
+//            rotationAmount = 360.0
+//
+//            timer.upstream.connect().cancel()
+//        }
     }
 }
 
