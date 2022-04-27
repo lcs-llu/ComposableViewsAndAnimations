e8c6034493cae0fda3e47b0951ce3d49beed0e47
Leo Lu <llu@lcs.on.ca>
2022-04-26 20:08:56 -0400

    Reformatted the code and made colourful waves around the robot

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -11,11 +11,13 @@ struct CustomComposableView: View {
     
     
     @State var xOffset = -100.0
-
+    
     @State var rotationAmount = 0.0
-
+    
+    @State var shouldAnimate = false
+    
     let timer = Timer.publish(every: 0.25, on: .main, in: .common).autoconnect()
-
+    
     var body: some View {
         VStack {
             Rectangle()
@@ -25,13 +27,32 @@ struct CustomComposableView: View {
                 Rectangle()
                     .frame(width: 40, height: 10)
                 
-        ZStack {
-            Rectangle()
-                .frame(width: 80, height: 80)
-            Text("Leo")
-                .foregroundColor(.white)
-            
-        }
+                ZStack {
+                    Rectangle()
+                        .frame(width: 80, height: 80)
+                        .overlay(
+                            ZStack {
+                                Circle()
+                                    .stroke(Color.yellow, lineWidth: 100)
+                                    .scaleEffect(shouldAnimate ? 1 : 0)
+                                Circle()
+                                    .stroke(Color.red, lineWidth: 100)
+                                    .scaleEffect(shouldAnimate ? 1.5 : 0)
+                                Circle()
+                                    .stroke(Color.blue, lineWidth: 100)
+                                    .scaleEffect(shouldAnimate ? 2 : 0)
+                            }
+                                .opacity(shouldAnimate ? 0.0 : 0.2)
+                                .animation(
+                                    Animation
+                                        .easeInOut(duration: 2)
+                                        .repeatForever(autoreverses: true)
+                                )
+                        )
+                    Text("Leo")
+                        .foregroundColor(.white)
+                    
+                }
                 Rectangle()
                     .frame(width: 40, height: 10)
             }
@@ -44,44 +65,44 @@ struct CustomComposableView: View {
             }
         }
         
-//        .rotationEffect(.degrees(rotationAmount), anchor:  .center)
+        .rotationEffect(.degrees(rotationAmount), anchor:  .center)
         .offset(x:xOffset, y:0)
-                 .animation(
-                    Animation.easeInOut(duration: 2)
-                        .repeatForever(autoreverses: true)
-                    )
-                 .onReceive(timer) { input in
-                 
-                             xOffset = 100.0
-                             rotationAmount = 360.0
-                 
-                             timer.upstream.connect().cancel()
-                         }
-//       ZStack {
-//
-//
-//
-//            Circle()
-//                .frame(width: 50, height: 50)
-//            Text("Ok")
-//                .foregroundColor(.white)
-//        }
-//
-//        .rotationEffect(.degrees(rotationAmount), anchor: .center)
-//        .offset(x:xOffset, y:0)
-//            .animation(
-//                Animation.easeInOut(duration: 2)
-//                    .repeatForever(autoreverses: true)
-//
-//        )
-//
-//        .onReceive(timer) { input in
-//
-//            xOffset = 100.0
-//            rotationAmount = 360.0
-//
-//            timer.upstream.connect().cancel()
-//        }
+        .animation(
+            Animation
+                .easeInOut(duration: 2)
+                .repeatForever(autoreverses: true)
+        )
+        .onReceive(timer) { input in
+            xOffset = 100.0
+            rotationAmount = 360.0
+            shouldAnimate = true
+            timer.upstream.connect().cancel()
+        }
+        //       ZStack {
+        //
+        //
+        //
+        //            Circle()
+        //                .frame(width: 50, height: 50)
+        //            Text("Ok")
+        //                .foregroundColor(.white)
+        //        }
+        //
+        //        .rotationEffect(.degrees(rotationAmount), anchor: .center)
+        //        .offset(x:xOffset, y:0)
+        //            .animation(
+        //                Animation.easeInOut(duration: 2)
+        //                    .repeatForever(autoreverses: true)
+        //
+        //        )
+        //
+        //        .onReceive(timer) { input in
+        //
+        //            xOffset = 100.0
+        //            rotationAmount = 360.0
+        //
+        //            timer.upstream.connect().cancel()
+        //        }
     }
 }
 
